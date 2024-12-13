86ab088992c1f0b82960e86677b1c8e6151337bb
miguel <mi_lmc@hotmail.com>
2024-12-12 23:17:04 -0300

    refactor: make background component accept styles

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,18 +1,19 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { ReactNode } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
 
 type BackgroundProps = {
   children: ReactNode;
+  style?: ViewStyle;
 };
 
-export default function Background({ children }: BackgroundProps) {
+export default function Background({ children, style }: BackgroundProps) {
   return (
     <LinearGradient
       colors={['#D700FF', '#83009B']}
       start={{ x: 0.9, y: 0.15 }}
       end={{ x: 0.25, y: 0.55 }}
-      style={styles.container}
+      style={{ ...styles.container, ...style }}
     >
       {children}
     </LinearGradient>
